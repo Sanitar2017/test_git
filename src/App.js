ae48515e91506fd048214ca4c833e452cfe7ad68
Vaso Pupkidze <radjawarior@mail.ru>
2022-03-23 01:10:19 +0200

    added components to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
 import './App.css';
+import LoginPage from "./components/LoginPage";
+import Action1 from "./components/Action1";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <LoginPage/>
+            <Action1/>
+        </div>
+    );
 }
 
 export default App;
